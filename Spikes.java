46a8a738a5e832fbf16a83916d83081d80fe059d
BRShooter957 <BRShooter957@gmail.com>
2017-03-27 13:49:08 -0700

    Update Spikes.java

--- Spikes.java
+++ Spikes.java
@@ -1,1 +1,20 @@
+package com.lira.speedfreak;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Spikes {
+    private static final float COLLISION_RECT_WIDTH = 13f;
+    private static final float COLLISION_RECT_HEIGHT = 447f;
+    private float x;
+    private float y;
+    private boolean pointClaimed = false;
+
+}
 
