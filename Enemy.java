79846fdae1a410ec4874e7c6575b97cd5abae61f
BRShooter957 <BRShooter957@gmail.com>
2017-03-27 13:46:37 -0700

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,1 +1,13 @@
+package com.lira.speedfreak;
+
+/**
+ * Created by llira18 on 3/21/2017.
+ */
+public class Enemy {
+    private static final float COLLISION_RECT_WIDTH=13f;
+    private static final float COLLISION_RECT_HEIGHT=447f;
+    private static final float COLLISION_RADIUS=33f;
+    private float x;
+    private float y;
+}
 
